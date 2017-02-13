a3c2ed686f6f090984e7bf0572821f93cebf5d76
Rob McKenzie <rob@>
2017-02-13 13:01:03 -0800

    add scripts for converting various unit types

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// gallons to liters
+var userGallons = parseInt(prompt("Enter number of gallons to convert: "));
+var calcToLiters = (userGallons * 3.785411784);
+alert(calcToLiters);
+
+// pounds to kg
+var userPounds = parseInt(prompt("Enter number of pounds to convert: "));
+var calcToKG = (userPounds * 0.45);
+alert(calcToKG);
+
+//cups to liters
+var userCups = parseInt(prompt("Enter number of cups to convert: "));
+var calcCupToLiters = (userCups * .236588);
+alert(calcCupToLiters);
